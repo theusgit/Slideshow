3569537104841197e1fed8bf2cdd29524fcab48e
theusgit <theusgit@outlook.com>
2021-01-26 17:28:58 -0300

    Iniciando projeto

---
+++ slideShow.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const images = [
+    { 'id': '1', 'url': './img/piz.jpg' },
+    { 'id': '2', 'url': './img/piz2.jpg' },
+    { 'id': '3', 'url': './img/piz3.jpg' },
+]
+
+
+const loadImages = (images, container) => {
+    
+}
+
+loadImages(images)
