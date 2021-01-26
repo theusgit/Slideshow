b76a908fe6a69c3452ca4d2f916ec24384f4049c
theusgit <theusgit@outlook.com>
2021-01-26 17:43:32 -0300

    imagens carregadas dinamicamente

--- slideShow.js
+++ slideShow.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const images = [
-    { 'id': '1', 'url': './img/piz.jpg' },
-    { 'id': '2', 'url': './img/piz2.jpg' },
-    { 'id': '3', 'url': './img/piz3.jpg' },
+    { 'id': '1', 'url': './img/piz.jpeg' },
+    { 'id': '2', 'url': './img/piz2.jpeg' },
+    { 'id': '3', 'url': './img/piz3.jpeg' },
 ]
 
 
-const loadImages = (images, container) => {
-    
+const container = document.querySelector('#container-items');
+
+
+
+
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
+    })
 }
 
-loadImages(images)
+loadImages(images, container)
